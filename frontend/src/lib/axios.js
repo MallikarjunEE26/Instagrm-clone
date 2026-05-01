09bb7b0a5fa5a4a1c1e2fc8d576b4f0545cc0b87
MallikarjunEE26 <mallikarjun.kumbar.cs@gmail.com>
2026-05-02 03:06:35 +0530

    update frontend API to deployed backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5002/api"
+    : "https://instagrm-clone.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
